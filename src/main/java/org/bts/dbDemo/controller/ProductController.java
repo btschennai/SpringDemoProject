b361e4d465af73fb018f8e02a0a0302a31821164
BTS Chennai <info.btschennai@gmail.com>
2026-07-15 18:46:22 +0530

    added CRUD operations

--- src/main/java/org/bts/dbDemo/controller/ProductController.java
+++ src/main/java/org/bts/dbDemo/controller/ProductController.java
@@ -1,9 +1,15 @@
 package org.bts.dbDemo.controller;
 
+import java.util.List;
+
 import org.bts.dbDemo.model.Products;
 import org.bts.dbDemo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +19,9 @@ public class ProductController {
 	@Autowired
 	ProductService service;
 	
-	public Products getProducts() {
-		return null;
+	@GetMapping("/listProducts")
+	public List<Products> getProducts() {
+		return service.getProducts();
 	}
 	
 	@PostMapping("/createProduct")
@@ -22,12 +29,15 @@ public class ProductController {
 		return service.addNewProduct(products);
 	}
 	
-	public Products updateProduct() {
-		return null;
+	@PutMapping("/updateProduct")
+	public Products updateProduct(@RequestBody Products products) {
+		return service.updateProduct(products);
 	}
 	
-	public Products deleteProduct() {
-		return null;
+	//http://localhost:8080/deleteProduct/1003
+	@DeleteMapping("/deleteProduct/{id}")
+	public void deleteProduct(@PathVariable("id")  long id) {
+		service.delete(id);
 	}
 
 }
--- src/main/java/org/bts/dbDemo/service/ProductService.java
+++ src/main/java/org/bts/dbDemo/service/ProductService.java
@@ -1,5 +1,7 @@
 package org.bts.dbDemo.service;
 
+import java.util.List;
+
 import org.bts.dbDemo.model.Products;
 import org.bts.dbDemo.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,18 @@ public class ProductService {
 	public Products addNewProduct(Products products) {
 		return repo.save(products);
 		// insert into products (productid, name, price, qty) values (products.getProductid, products.getName, products.getPrice, products.getqty)
+	}
+	
+	public List<Products> getProducts() {
+			return repo.findAll();
+	}
+	
+	public Products updateProduct(Products product) {
+		return repo.save(product);
+	}
+	
+	public void delete(long id) {
+		repo.deleteById(id);
 	}
 
 }
